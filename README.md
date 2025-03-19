f2075e1be8d7e615eec19d3fdd7ec43a2a011164
James Iyouboushi <Iyouboushi@users.noreply.github.com>
2025-03-19 12:40:46 -0500

    version 1.6.0
    version 1.6.0 of the bot

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Kon
-Kon the console C# Chat Bot
+# Kon version 1.6.0
+
+"Kon" is my attempt at creating a "chatterbot" written in C#. This bot will 
+connect to an IRC server and join a channel whereupon it will "watch" the 
+conversations. It will then try to "learn" from them so that people can hold 
+conversations with it.
+
+It has several different "brains" to learn/use in conversations. It has a 
+basic brain that is simply a "monkey-see, monkey-do." This brain will repeat 
+something someone has said earlier in full, without manipulating it. And it
+has a "Language Bot" brain which is based on a chatterbot called "Language Bot" 
+(written by BishounenNightBird of Esper.net). This brain attempts to take 
+what people says and manipulate it so it can create semi-original thoughts. 
+This brain is currently set to default.
+
+# Setup
+Setup is simple. Clone or download the kon.exe, LB Brain Utility.exe
+kon.brain, lb.brain, config.xml, and throwitems.txt files and put them
+into a folder.  Then edit the config file.  
+
+Open config.xml in notepad or whatever editor you would like to use,
+and change the values for your IRC server, port, the bot's nick, the
+default channel you would like the bot to join, and what value you
+would like to set "Random Talk" (the bot's ability to speak randomly
+without being addressed).  Finally you will want to set your nick as
+the admin.
+
+After you have finished editing the config file, just double click 
+kon.exe and it will automatically join the server and channel you
+have set.
+
+To address the bot, use
+
+Kon: text goes here
+
+OR
+
+YourBot'sName: text goes here
+
+
+** Note: although the bot comes with a small LB Brain filled with some
+Internet memes and other sayings, until the bot views some conversations 
+it will still have very little to say.
---
+++ src/logger.cs
@@ -0,0 +1,66 @@
+////////////////////////////////////////////////
+////////////////PROGRAM INFO////////////////////
+// "Kon" is a "learning bot" written in C#.   //
+// The bot will connect to an IRC server and  //
+// "watch" the conversations.  It will then   //
+// "learn" from them.  You'll then be able to //
+// talk to it and hopefully it'll hold a conv-//
+// ersation.                                  //
+////////////////////////////////////////////////
+// "Kon" is an ongoing project started by     // 
+// James Iyouboushi.                          //
+// Email: Iyouboushi@gmail.com                //
+////////////////////////////////////////////////
+// This file was last updated on: 03/19/2025  //
+////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Kon
+{
+    partial class IRC
+    {
+
+#region logger
+        ////////////////////////////////////////////////
+        ///////////////////LOGGER///////////////////////
+        ////////////////////////////////////////////////
+        // Dump the lines into a log file for later   //
+        // review.                                    //
+        ////////////////////////////////////////////////
+        private static void LogFileOn()
+        {
+            // If logging is enabled, let's turn it on and indicate what time we started
+            if (logging == true)
+            {
+                logger = new StreamWriter(LOG_FILE, true);
+                LogFile("---------[STARTING: " + DateTime.Now + "]");
+            }
+        }
+
+        private static void LogFile(string line)
+        {
+            if (logging)
+            {
+                try
+                {
+                    logger.WriteLine(line);
+                    logger.Flush();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    LogFileOn();
+                }
+            }
+        }
+#endregion
+
+    }
+}
